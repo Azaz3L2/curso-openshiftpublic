e41fbf36453226ec9e52a78793aa2af7b40ee483
Azaz3L2 <67270720+Azaz3L2@users.noreply.github.com>
2020-06-23 11:43:31 +0200

    optimista

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-  return "Bienvenido 2!!!"
+  return "Bienvenido v3!!!"
   
 @app.route('/como estas')
 def hola():
-  return 'Estoy bien, gracias'
+  return 'Estoy super bien, gracias'
   
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080)
